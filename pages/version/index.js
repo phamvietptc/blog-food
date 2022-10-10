a5c64a1ea7b01467afa7b22394d162b49fc38926
Tien Pham <tien.phamnguyen@powergatesoftware.com>
2022-10-10 15:34:18 +0700

    error Version Page

--- pages/version/index.js
+++ pages/version/index.js
@@ -1,16 +1,10 @@
-import Image from 'next/image'
-import Navbar, { MBFooter, Footer } from '../navbar'
-import Link from 'next/link'
 import styles from '../../../styles/Home.module.css'
-import { useRouter } from 'next/router'
 
 export default function Version() {
   return (
     <>
       <div className={styles.contain}>
-        <div className={styles.content}>
-			Version 0.1
-        </div>
+        <div className={styles.content}>Version 0.1</div>
       </div>
     </>
   )
