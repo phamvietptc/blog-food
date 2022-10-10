ee7d15cb26c38d654913724fff11c766a5591a4f
Tien Pham <tien.phamnguyen@powergatesoftware.com>
2022-10-10 15:28:22 +0700

    change name Version Page

--- pages/views/Version/index.js
+++ pages/views/Version/index.js
@@ -4,7 +4,7 @@ import Link from 'next/link'
 import styles from '../../../styles/Home.module.css'
 import { useRouter } from 'next/router'
 
-export default function Home() {
+export default function Version() {
   return (
     <>
       <div className={styles.contain}>
