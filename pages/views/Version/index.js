95e0aca21b40c080c3d1e14fca2f87a580190e2d
Tien Pham <tien.phamnguyen@powergatesoftware.com>
2022-10-10 11:04:12 +0700

    Add Version Page

---
+++ pages/views/Version/index.js
@@ -0,0 +1,17 @@
+import Image from 'next/image'
+import Navbar, { MBFooter, Footer } from '../navbar'
+import Link from 'next/link'
+import styles from '../../../styles/Home.module.css'
+import { useRouter } from 'next/router'
+
+export default function Home() {
+  return (
+    <>
+      <div className={styles.contain}>
+        <div className={styles.content}>
+			Version 0.1
+        </div>
+      </div>
+    </>
+  )
+}
