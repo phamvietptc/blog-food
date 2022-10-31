5298410751ee182a118182cb1ff3ee37f891023f
phamvietptc <phamvietptc@gmail.com>
2022-11-01 00:41:18 +0700

    dropdown

--- pages/views/navbar.js
+++ pages/views/navbar.js
@@ -139,6 +139,13 @@ export function Menu() {
     <>
     <div className={`md:hidden ${NavbarStyles.menu}`}>
       <div className="dropdown dropdown-end">
+
+      {/* <label tabIndex={0} className="btn border-transparent bg-inherit  swap swap-rotate">
+        <input type="checkbox" />        
+        <svg className="bg-inherit swap-off" xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 512 512"><path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z"/></svg>       
+        <svg className="bg-inherit swap-on" xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 512 512"><polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49"/></svg>
+      </label> */}
+
         <label tabIndex={0}>
           <Image
               width='24px'
