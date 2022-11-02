af2d45e5fcef21005920bed1e989799af5c75146
phamvietptc <phamvietptc@gmail.com>
2022-11-03 00:29:47 +0700

    tailwind UI pages Categories

--- pages/views/Categories/index.js
+++ pages/views/Categories/index.js
@@ -43,18 +43,18 @@ export default function Categories() {
 
     return (
         <>
-        <div className={CategoriesStyles.container}>
+        <div className='bg-white box-border outline-none p-0'>
           <Navbar />
           <div className={CategoriesStyles.content}>
-            <div className={CategoriesStyles.categories}>
-                <h5 className={CategoriesStyles.headText}>Categories</h5>
+            <div className='my-6 md:my-12 md:mx-auto md:max-w-6xl'>
+                <h5 className='text-black text-2xl leading-tight font-bold font-serif py-4 mt-0 mb-4 border-b border-inherit border-solid md:text-6xl md:leading-tight md:mb-12'>Categories</h5>
                 <div className={CategoriesStyles.rowCategories}>
                     {content.map((item, index) => (
                     <div key={index} className={CategoriesStyles.blockCategories}>
-                        <div  className={CategoriesStyles.imgCategories}>
-                            <Image className={CategoriesStyles.image} width='255px' height='255px' src={item.image} alt="" />
-                        <figcaption className={CategoriesStyles.titleCategories}>
-                            <a className={CategoriesStyles.textCategories}>{item.title}</a>
+                        <div  className='relative text-center my-4 md:my-6'>
+                            <Image className='overflow-hidden rounded-full max-w-full cursor-pointer' width='255px' height='255px' src={item.image} alt="" />
+                        <figcaption className='mt-2 md:mt-4'>
+                            <a className='text-black text-sm font-sans font-semibold mt-2 text-center cursor-pointer md:text-xl md:leading-normal'>{item.title}</a>
                         </figcaption>
                         </div>
                     </div>
