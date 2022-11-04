8fbd97c9aef11cca3782003a8ab79b148983c61d
phamvietptc <phamvietptc@gmail.com>
2022-11-04 17:29:31 +0700

    tailwind UI pages Recipes

--- pages/views/Recipes/index.js
+++ pages/views/Recipes/index.js
@@ -1,6 +1,7 @@
 import RecipesStyles from '../../../styles/Recipes.module.css';
 import Navbar, { MBFooter, Footer } from '../navbar';
 import { useRouter } from 'next/router';
+import Image from 'next/image';
 
 export default function Recipe() {
     return (
@@ -171,108 +172,108 @@ export function Posts() {
 
     return (
         <>
-            <div className={RecipesStyles.recipes}>
-                <div className={RecipesStyles.head}>
-                    <div className={RecipesStyles.iconsHead}>
-                        <div className={RecipesStyles.iconsBlock}>
+            <div className='my-6 md:my-12'>
+                <div className='outline-none md:flex'>
+                    <div className='ml-auto md:order-2'>
+                        <div className='flex pt-0 justify-end -mb-6'>
                             <a
-                             className={RecipesStyles.iconsShare}
+                             className='ml-4 md:ml-9 cursor-pointer'
                              href='/'
                             >
-                                <img className={RecipesStyles.share} src='/icons/recipes/icons-share.png' />
+                              <img className='w-5 h-5 md:h-8 md:w-8 align-middle cursor-pointer' src='/icons/recipes/icons-share.png' />
                             </a>
                             <a
-                             className={RecipesStyles.iconsShare}
+                             className='ml-4 md:ml-9 cursor-pointer'
                              href='/'
                             >
-                                <img className={RecipesStyles.share} src='/icons/recipes/icons-tag.png' />
+                                <img className='w-5 h-5 md:h-8 md:w-8 align-middle cursor-pointer' src='/icons/recipes/icons-tag.png' />
                             </a>
                         </div>
                     </div>
                     <div>
                         <strong>
-                            <img className={RecipesStyles.chart} src='/icons/recipes/icons-chart.png'/>
-                            <span className={RecipesStyles.spanHead}>85% would make this again</span>
+                            <img className='inline align-middle w-5 h-5' src='/icons/recipes/icons-chart.png'/>
+                            <span className='text-black font-sans text-xs font-medium ml-2 md:text-sm leading-normal'>85% would make this again</span>
                         </strong>
-                        <h5 className={RecipesStyles.textHead}>Strawberry Cream Cheesecake</h5>
+                        <h5 className='text-black text-2xl font-bold font-serif leading-tight py-4 my-0 md:text-6xl'>Strawberry Cream Cheesecake</h5>
                     </div>
                 </div>
 
-                <div className={RecipesStyles.author}>
-                    <div className={RecipesStyles.boxAuthor}>
-                        <img className={RecipesStyles.imgAuthor} src='/images/recipes/avatar-author.png' />
-                        <small className={RecipesStyles.textAuthor}>Tricia Albert</small>
+                <div className='flex flex-wrap md:leading-normal'>
+                    <div className='my-2 mr-6'>
+                        <img className='inline rounded-full max-w-full align-middle' src='/images/recipes/avatar-author.png' />
+                        <small className='text-black text-xs font-sans font-normal leading-normal pl-1'>Tricia Albert</small>
                     </div>
-                    <div className={RecipesStyles.boxAuthor}>
-                        <img className={RecipesStyles.iconsAuthor} src='/icons/recipes/icons-calendar.png' />
-                        <small className={RecipesStyles.textAuthor}>Yesterday</small>
+                    <div className='my-2 mr-6'>
+                        <img className='inline align-middle w-5 h-5' src='/icons/recipes/icons-calendar.png' />
+                        <small className='text-black text-xs font-sans font-normal leading-normal pl-1'>Yesterday</small>
                     </div>
-                    <div className={RecipesStyles.boxAuthor}>
-                        <img className={RecipesStyles.iconsAuthor} src='/icons/recipes/icons-number-comment.png' />
-                        <small className={RecipesStyles.textAuthor}>25</small>
+                    <div className='my-2 mr-6'>
+                        <img className='inline align-middle w-5 h-5' src='/icons/recipes/icons-number-comment.png' />
+                        <small className='text-black text-xs font-sans font-normal leading-normal pl-1'>25</small>
                     </div>
-                    <div className={RecipesStyles.voted}>
+                    <div className='my-2 w-full float-left leading-normal font-bold text-red-500 md:w-fit'>
                         ★★★★★
                     </div>
                 </div>
 
                 <div class='detail'>
                     <hr style={{borderColor:'#E8E8E8', backgroundColor:'currentcolor', color:'inherit', opacity:'0.3', height:'1px', margin:'20px 0'}} />
-                    <p className={RecipesStyles.preface}>One thing I learned living in the Canarsie section of Brooklyn, NY was how to cook a good Italian meal. Here is a recipe I created after having this dish in a restaurant. Enjoy!</p>
+                    <p className='text-black text-sm leading-6 font-normal font-sans mt-0 mb-4 md:text-xl md:leading-9'>One thing I learned living in the Canarsie section of Brooklyn, NY was how to cook a good Italian meal. Here is a recipe I created after having this dish in a restaurant. Enjoy!</p>
                     <br />
-                    <img className={RecipesStyles.imgIntro} src='/images/recipes/intro-images.jpg' />
+                    <img className='relative rounded-xl overflow-hidden w-full max-w-full align-middle' src='/images/recipes/intro-images.jpg' />
                     <br />
                     <div className={RecipesStyles.blockDetail}>
                         <div className={RecipesStyles.leftBlock}>
-                            <div className={RecipesStyles.inLeftBlock}>
-                                <ul className={RecipesStyles.foodPlan}>
-                                    <li className={RecipesStyles.headPlan}>
-                                        <small className={RecipesStyles.textPlan} >PREP TIME</small>
-                                        <span className={RecipesStyles.timePlan}>15 MIN</span>
+                            <div className='pr-0 md:pr-12'>
+                                <ul className='flex flex-wrap pl-0 list-none md:-ml-7'>
+                                    <li className='py-1 px-2 md:px-7'>
+                                        <small className='text-gray-500 text-xs leading-3 font-sans font-normal block'>PREP TIME</small>
+                                        <span className='text-black text-sm font-sans font-normal leading-normal'>15 MIN</span>
                                     </li>
-                                    <li className={RecipesStyles.plan}>
-                                        <small className={RecipesStyles.textPlan}>PREP TIME</small>
-                                        <span className={RecipesStyles.timePlan}>15 MIN</span>
+                                    <li className='py-1 px-2 md:px-7 border-l border-solid border-inherit'>
+                                        <small className='text-gray-500 text-xs leading-3 font-sans font-normal block'>PREP TIME</small>
+                                        <span className='text-black text-sm font-sans font-normal leading-normal'>15 MIN</span>
                                     </li>
-                                    <li className={RecipesStyles.plan}>
-                                        <small className={RecipesStyles.textPlan}>SERVINGS</small>
-                                        <span className={RecipesStyles.timePlan}>
+                                    <li className='py-1 px-2 md:px-7 border-l border-solid border-inherit'>
+                                        <small className='text-gray-500 text-xs leading-3 font-sans font-normal block'>SERVINGS</small>
+                                        <span className='text-black text-sm font-sans font-normal leading-normal'>
                                             4 PEOPLE
-                                            <img className={RecipesStyles.iconsServings} src='/icons/recipes/icons-servings.png' />
+                                            <img className='inline ml-2 cursor-pointer align-middle w-5 h-5 md:w-6 md:h-6' src='/icons/recipes/icons-servings.png' />
                                         </span>
                                     </li>
-                                    <li className={RecipesStyles.print}>
-                                        <img className={RecipesStyles.iconsPrint} src='/icons/recipes/printer.svg' />
+                                    <li className='py-1 px-2 md:px-7 border-l border-solid border-inherit'>
+                                        <img className='cursor-pointer align-middle w-5 h-5 md:w-6 md:h-6' src='/icons/recipes/printer.svg' />
                                     </li>
                                 </ul>
-                                <div className={RecipesStyles.ingredients}>
-                                    <h6 className={RecipesStyles.headIngredients}>Ingredients</h6>       
-                                    <div className={RecipesStyles.cake}>
-                                        <strong className={RecipesStyles.titleCake}>For the crust</strong>
+                                <div className='mt-6 mt-12'>
+                                    <h6 className='text-black text-xl font-bold font-serif leading-tight mt-0 mb-2 md:text-3xl'>Ingredients</h6>       
+                                    <div className='mt-6 pb-2'>
+                                        <strong className='text-black text-sm font-sans font-semibold leading-normal md:text-lg'>For the crust</strong>
                                         {cakeCrust.map((item, index) => (
-                                        <div key={index} className={RecipesStyles.tickCake}>
+                                        <div key={index} className='block mt-6 mb-0.5'>
                                             <input type="checkbox" className="checkbox checkbox-accent rounded-full float-left mr-3" />
-                                            <label className={RecipesStyles.textCake}>{item.text}</label>
+                                            <label className='text-black text-lg font-sans font-normal leading-tight cursor-pointer block pl-9'>{item.text}</label>
                                         </div>
                                         ))}
                                     </div>
                                     
-                                    <div className={RecipesStyles.cake}>
-                                        <strong className={RecipesStyles.titleCake}>For the cheescake</strong>
+                                    <div className='mt-6 pb-2'>
+                                        <strong className='text-black text-sm font-sans font-semibold leading-normal md:text-lg'>For the cheescake</strong>
                                         {cake.map((item, index) => (
-                                        <div key={index} className={RecipesStyles.tickCake}>
+                                        <div key={index} className='block mt-6 mb-0.5'>
                                             <input type="checkbox" className="checkbox checkbox-accent rounded-full float-left mr-3" />
-                                            <label className={RecipesStyles.textCake}>{item.text}</label>
+                                            <label className='text-black text-lg font-sans font-normal leading-tight cursor-pointer block pl-9'>{item.text}</label>
                                         </div>
                                         ))}
                                     </div>
                                 </div>
-                                <div className={RecipesStyles.instructions}>
-                                    <h6 className={RecipesStyles.titleInstructions}>Instructions</h6>
+                                <div className='mt-4 mb-16 md:mt-12'>
+                                    <h6 className='text-black text-xl font-bold font-serif leading-tight mt-0 mb-2 md:text-3xl md:leading-tight'>Instructions</h6>
                                     {instructions.map((item, index) => (
-                                    <ul key={index} className={RecipesStyles.listInstructions}>
-                                        <li className={RecipesStyles.textInstructions}>
-                                            <span className={RecipesStyles.numberInstructions}>{item.id}</span>
+                                    <ul key={index} className='pt-3.5 pl-0 mt-0 list-none md:mb-4'>
+                                        <li className='relative text-black font-sans font-normal text-sm leading-6 pr-0 pl-9 md:text-lg md:leading-normal md:pr-7 md:pl-10'>
+                                            <span className='absolute top-0.5 left-0 w-6 h-6 rounded-full bg-orange-600 font-medium text-white text-sm text-center flex justify-center items-center flex-col'>{item.id}</span>
                                             {item.text}
                                         </li>
                                     </ul>
