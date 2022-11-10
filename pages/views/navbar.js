265f1e9e0289bc5d02e43861cabc9e84d867a659
phamvietptc <phamvietptc@gmail.com>
2022-11-10 17:39:20 +0700

    10/11/2022

--- pages/views/navbar.js
+++ pages/views/navbar.js
@@ -110,6 +110,31 @@ export function Search_MB() {
     <button className="md:hidden btn btn-ghost btn-circle ml-36">
       <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-neutral" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
     </button>
+
+
+    <div className={NavbarStyles.search}>
+      <div className={NavbarStyles.containerSearch}>
+        <div className={NavbarStyles.inputSearch}>
+          <input type="text" name="Search" placeholder="Search" class={NavbarStyles.form} id="Search"/>
+          <button type="button" className={NavbarStyles.buttonS}>
+            <img className={NavbarStyles.im} src="/icons/navbar/icons-close.svg" alt="img"/>
+          </button>
+        </div>
+        <div className={NavbarStyles.result} class="search-results" id="SearchList">
+          <div class="search-list">
+            <a href='#0' className={NavbarStyles.aaaa}>
+              <figure className={NavbarStyles.figure}>
+                <img className={NavbarStyles.mm} src="/images/navbar/search.png" class="rounded-circle" alt="Menu"/>
+              </figure>
+              <div className={NavbarStyles.namee} class="tstbite-search-name">
+                <strong className={NavbarStyles.small} class="small">Cake</strong>
+                <span className={NavbarStyles.tiny} class="tiny">Category</span>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
     </>
   )
 }
