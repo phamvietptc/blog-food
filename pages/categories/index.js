7669d38ac1872b80cef6b7d8c83c8a846f632381
phamvietptc <phamvietptc@gmail.com>
2022-11-29 16:33:37 +0700

    api

--- pages/categories/index.js
+++ pages/categories/index.js
@@ -8,77 +8,21 @@ import api from '../api';
 
 export default function Categories() {
 
-    const content = [
-        {
-          image: '/images/categories/categories-img.png',
-          title: 'Seafood',
-        },
-        {
-          image: '/images/categories/categories-img.png',
-          title: 'Soup',
-        },
-        {
-          image: '/images/categories/categories-img.png',
-          title: 'Pancakes',
-        },
-        {
-          image: '/images/categories/categories-img.png',
-          title: 'Meat',
-        },
-        {
-          image: '/images/categories/categories-img.png',
-          title: 'Chicken',
-        },
-        {
-          image: '/images/categories/categories-img.png',
-          title: 'Pasta',
-        },
-        {
-          image: '/images/categories/categories-img.png',
-          title: 'Pizza',
-        },
-        {
-          image: '/images/categories/categories-img.png',
-          title: 'Less than 30 min',
-        },
-      ];
-
-      const [categoriesData, setcategoriesData] = useState([]);
-
-      useEffect(() => {
-   const getCategories = async () => {
-     const res = await api.categories.getCategoryDetail();
-     if (res.data == null) {
-       return;
-     }
-     setcategoriesData(res.data);
-   };
-   // const getCategories = async () => {
-   //   const res = await api.categories.getCategoryDetail();
-   //   if (res?.data == null) {
-   //     return;
-   //   }
-   //   console.log(123, teacherList);
-   //   setExperts(
-   //     teacherList.data.map((teacher) => ({
-   //       id: teacher.id,
-   //       url: teacher.avatar,
-   //       name: teacher.fullName,
-   //       role: teacher.title,
-   //     }))
-   //   );
-   //   setExpertsMobile(
-   //     teacherList.data.map((teacher) => ({
-   //       id: teacher.id,
-   //       url: teacher.avatar,
-   //       name: teacher.fullName,
-   //       role: teacher.title,
-   //     }))
-   //   );
-   // };
+const [categoriesData, setcategoriesData] = useState([]);
+useEffect(() => {
+  const getCategories = async () => {
+    const res = await api.categories.getCategoryDetail();
+    const buttonMenu = [
+    ...res?.data?.map((item) => ({
+    id: item.id,
+    Title: item.title,
+    img: item.image,
+  })),
+  ];
+  setcategoriesData(buttonMenu);
+};
    getCategories();
-   // getTeachers();
- }, []);
+}, []);
 
     return (
         <>
@@ -88,14 +32,14 @@ export default function Categories() {
             <div className='my-6 md:my-12 md:mx-auto md:max-w-6xl'>
                 <h5 className='text-black text-2xl leading-tight font-bold font-serif py-4 mt-0 mb-4 border-b border-inherit border-solid md:text-6xl md:leading-tight md:mb-12'>Categories</h5>
                 <div className={CategoriesStyles.rowCategories}>
-                    {categoriesData.map((id, index) => (
-                    <div key={index} className={CategoriesStyles.blockCategories}>
+                    {categoriesData.map((item, index) => (
+                    <div key={index} id={item.id} className={CategoriesStyles.blockCategories}>
                       <div  className='relative text-center my-4 md:my-6 text-black hover:text-red-500'>
                         <div className='hover:scale-105 transition-all'>
-                          <Image className='overflow-hidden rounded-full max-w-full cursor-pointer' width='255px' height='255px' src={id.image} alt="Menu"/>
+                          <Image className='overflow-hidden rounded-full max-w-full cursor-pointer' width='255px' height='255px' src={item.image} alt="Menu"/>
                         </div>
                         <figcaption className='mt-2 md:mt-4'>
-                          <a className='text-sm font-sans font-semibold mt-2 text-center cursor-pointer md:text-xl md:leading-normal'>{id.title}</a>
+                          <a className='text-sm font-sans font-semibold mt-2 text-center cursor-pointer md:text-xl md:leading-normal'>{item.title}</a>
                         </figcaption>
                       </div>
                     </div>
